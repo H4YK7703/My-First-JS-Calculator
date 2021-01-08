3303f57059f8c5adbd3831aeca68a6b30dc945de
Hayk Melqonyan <h4ykm3lqonyan@gmail.com>
2021-01-08 16:41:34 +0300

    Finel Version

---
+++ d
@@ -0,0 +1,23 @@
+[33mcommit 63434a53b08f7421d77ef875d5147a6f1a88ed07[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Hayk Melqonyan <h4ykm3lqonyan@gmail.com>
+Date:   Fri Jan 8 16:22:23 2021 +0300
+
+    bug_fix_02 - js
+
+[33mcommit 66c1f03cb3f7667ebbea94109a55253953adba37[m
+Author: Hayk Melqonyan <h4ykm3lqonyan@gmail.com>
+Date:   Fri Jan 8 16:12:42 2021 +0300
+
+    Bug_01 Fix
+
+[33mcommit a6b741fda91cf4e9076d04a639b502793c4837ea[m
+Author: Hayk Melqonyan <h4ykm3lqonyan@gmail.com>
+Date:   Fri Jan 8 16:10:35 2021 +0300
+
+    Calculator Example
+
+[33mcommit e0561772dc3a494bdf63014021bcc757db702437[m
+Author: Hayk Melqonyan <h4ykm3lqonyan@gmail.com>
+Date:   Fri Jan 8 16:06:11 2021 +0300
+
+    First , Default Commit
--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-alert("Welcome to The Online Calculator");
+alert("Welcome to The Online Calculator!");
 
-const ansW = confirm("Do You Want Calculate Now?", "Yes");
+const calFirst = +prompt("Write Your First Number");
+const exType = +prompt("What You Want(1 - + , 2 - - , 3 - * , 4 - /)");
+const calTwo = +prompt("Write Your Second Number");
 
-let calFirst = +prompt("Write your first Number");
-let calTwo = +prompt("Write your Second Number");
-
-let exType = +prompt("What You Want| 1-+,2--,3-*,4-/");
-
-if(exType == 1) {
-    alert(calFirst+calTwo);
-} else if(exType == 2) {
-    alert(calFirst-calTwo);
-} else if(exType == 3){
-    alert(calFirst*calTwo);
-} else if(exType == 4){
-    alert(calFirst/calTwo);
-}
-else{
-    alert("Error");
+try {
+    if (exType == 1) {
+        alert(calFirst + calTwo);
+    } else if (exType == 2) {
+        alert(calFirst - calTwo);
+    } else if (exType == 3) {
+        alert(calFirst * calTwo);
+    } else if (exType == 4) {
+        alert(calFirst / calTwo);
+    } else {
+        alert("Error");
+    }
+} catch (err) {
+    alert("What is the Going Wrong , Try Again!");
 }

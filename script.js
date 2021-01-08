a6b741fda91cf4e9076d04a639b502793c4837ea
Hayk Melqonyan <h4ykm3lqonyan@gmail.com>
2021-01-08 16:10:35 +0300

    Calculator Example

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("Welcome to The Online Calculator");
+
+const ansW = confirm("Do You Want Calculate Now?" , "Yes");
+
+if (ansW == 1) {
+    const calCul = +prompt("Write your Expression");
+}
+
+alert("calCul");
